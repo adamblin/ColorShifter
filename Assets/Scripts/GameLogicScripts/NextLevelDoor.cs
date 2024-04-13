131624bea0ebb7644a6bb1f700c24eb40b81bfe4
RubenMailloBaena <121771440+RubenMailloBaena@users.noreply.github.com>
2024-04-13 11:16:32 +0200

    canges

---
+++ Assets/Scripts/GameLogicScripts/NextLevelDoor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelDoor : MonoBehaviour
+{
+    [SerializeField] private int doorHits;
+    [SerializeField] private string nextLevelName;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.red;
+    }
+
+    private void DoorCollided()
+    {
+        doorHits--;
+
+        if (doorHits <= 0) { 
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            spriteRenderer.color = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+
+
+}
--- Assets/Scripts/PlayerScripts/TongueController.cs
+++ Assets/Scripts/PlayerScripts/TongueController.cs
@@ -114,6 +114,7 @@ public class TongueController : MonoBehaviour
                     canCheckCollisions = false;
                     ChangeObjectEffect(hitColliders[i].gameObject);
                 }
+                
             }
         }
     }
