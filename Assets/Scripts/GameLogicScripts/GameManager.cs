9803a0d2c660a4fe91e0447e48f9ea77d5151eb1
RubenMailloBaena <121771440+RubenMailloBaena@users.noreply.github.com>
2024-04-29 10:49:29 +0200

    push

--- Assets/Scripts/GameLogicScripts/GameManager.cs
+++ Assets/Scripts/GameLogicScripts/GameManager.cs
@@ -9,16 +9,40 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance
     {
         get { 
-            if(instance == null)
+            if (instance == null)
                 instance = FindAnyObjectByType<GameManager>();
             return instance;
         }
     }
 
+
     [SerializeField] private List<GameObject> checkPoints;
     private int currentIndex = 0;
 
     public event Action<ColorType> onPlayerDeath;
+
+
+    [SerializeField] private GameObject pauseMenuPrefab;
+    private bool gamePaused = false;
+
+    private void Update()
+    {
+        ManagePauseGame();    
+    }
+
+    private void ManagePauseGame() {
+        Debug.Log(pauseMenuPrefab + " " + gamePaused);
+
+        if (gamePaused) {
+            pauseMenuPrefab.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else {
+            pauseMenuPrefab.SetActive(false);
+            Time.timeScale = 1f;  
+        }
+    }
+
 
     public void MoveToCheckPoint() {
         GameObject player = GameObject.Find("Player");
@@ -32,13 +56,20 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void PauseGame() {
+        gamePaused = !gamePaused;
+    }
+    
+
     private void OnEnable()
     {
         CheckPoint.onCheckPoint += nextCheckPoint;
+        PlayerInputs.Instance.onPauseGame += PauseGame;
     }
 
     private void OnDisable()
     {
         CheckPoint.onCheckPoint -= nextCheckPoint;
+        PlayerInputs.Instance.onPauseGame -= PauseGame;
     }
 }
