b9315bc527200118fefd149d32fd137552b8f9d8
RubenMailloBaena <121771440+RubenMailloBaena@users.noreply.github.com>
2024-05-01 12:41:55 +0200

    Update PlayerSquashed.cs

--- Assets/Scripts/PlayerScripts/PlayerSquashed.cs
+++ Assets/Scripts/PlayerScripts/PlayerSquashed.cs
@@ -6,10 +6,21 @@ public class PlayerSquashed : MonoBehaviour
     [Header("CHECK TOP AND BOTTOM")]
     [SerializeField] private float topXSize;
     [SerializeField] private float topYSize;
+    [SerializeField] private float topOffset;
+    
+
+    [SerializeField] private float bottomXSize;
+    [SerializeField] private float bottomYSize;
+    [SerializeField] private float bottomOffset;
 
     [Header("CHECK LATERALS")]
-    [SerializeField] private float latXSize;
-    [SerializeField] private float latYSize;
+    [SerializeField] private float leftXSize;
+    [SerializeField] private float leftYSize;
+    [SerializeField] private float leftOffset;
+
+    [SerializeField] private float rightXSize;
+    [SerializeField] private float rightYSize;
+    [SerializeField] private float rightOffset;
 
     [Header("WHAT CAN GET SQUASHED WITH")]
     [SerializeField] private LayerMask layerMask;
@@ -21,32 +32,59 @@ public class PlayerSquashed : MonoBehaviour
     }
 
     private void CheckIfPlayerSquished() {
-        Collider2D[] topAndBottomCol = Physics2D.OverlapBoxAll(transform.position, new Vector2(topXSize, topYSize), 0, layerMask);
-        CheckIfKillPlayer(topAndBottomCol);
+        Collider2D[] topCol = Physics2D.OverlapBoxAll(transform.position + new Vector3(0, transform.localScale.y + topOffset, 0), new Vector2(topXSize, topYSize), 0, layerMask);
+        Collider2D[] botCol = Physics2D.OverlapBoxAll(transform.position - new Vector3(0, transform.localScale.y + bottomOffset, 0), new Vector2(topXSize, topYSize), 0, layerMask);
+        if(topCol.Length != 0 && botCol.Length != 0)
+            CheckIfKillPlayer(topCol[0], botCol[0]);
 
-        Collider2D[] lateralCol = Physics2D.OverlapBoxAll(transform.position, new Vector2(latXSize, latYSize), 0, layerMask);
-        CheckIfKillPlayer(lateralCol);
+        Collider2D[] leftCol= Physics2D.OverlapBoxAll(transform.position - new Vector3(transform.localScale.x / 2 + leftOffset, 0, 0), new Vector2(leftXSize, leftYSize), 0, layerMask);
+        Collider2D[] rightCol = Physics2D.OverlapBoxAll(transform.position + new Vector3(transform.localScale.x / 2 + rightOffset, 0, 0), new Vector2(topXSize, topYSize), 0, layerMask);
+        if(leftCol.Length != 0 && rightCol.Length != 0)
+            CheckIfKillPlayer(topCol[0], botCol[0]);
 
-        Debug.Log("top: " + topAndBottomCol.Length + " lateral: " + lateralCol.Length);
+        Debug.Log("topCol: " + topCol.Length);
+        Debug.Log("botCol: " + botCol.Length);
+        Debug.Log("leftCol: " + leftCol.Length);
+        Debug.Log("rightCol: " + rightCol.Length);
     }
 
-    private void CheckIfKillPlayer(Collider2D[] collisions) {
-        if (collisions.Length >= 2) {
-            for (int i = 0; i < collisions.Length; i++){ //si estem chocant amb una paret y un obstacle de color groc
-                GameObject obstacle = collisions[i].gameObject;
+    private void CheckIfKillPlayer(Collider2D col1, Collider2D col2) {
+        Debug.Log("IN");
+        
+        
 
-                if (obstacle.CompareTag("PaintableObstacle"))
-                    if(obstacle.GetComponent<ObstacleEffectLogic>().getCurrentColorType() == ColorType.Strech)
-                        GameManager.Instance.MoveToCheckPoint();
-            }
-        }
+        if(col1.gameObject.)
+            GameManager.Instance.MoveToCheckPoint();
+
+        //if (collisions.Length >= 2) {
+        //    int counter = 0;
+        //    for (int i = 0; i < collisions.Length; i++){ //si estem chocant amb una paret y un obstacle de color groc
+        //        GameObject obstacle = collisions[i].gameObject;
+
+        //        if (obstacle.CompareTag("PaintableObstacle")) {
+        //            ColorType obstacleColorType = obstacle.GetComponent<ObstacleEffectLogic>().getCurrentColorType();
+        //            if (obstacleColorType == ColorType.Strech)
+        //                counter++;
+        //        }
+        //        else
+        //            counter++;
+        //    }
+        //    Debug.Log(counter);
+        //    if (counter >= 2) {
+        //        Debug.Log("Squashed");
+        //        GameManager.Instance.MoveToCheckPoint();
+        //    }
+                
+        //}
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector2(topXSize, topYSize));
-        Gizmos.DrawWireCube(transform.position, new Vector2(latXSize, latYSize));
+        Gizmos.DrawWireCube(transform.position + new Vector3(0, transform.localScale.y + topOffset, 0), new Vector2(topXSize, topYSize));
+        Gizmos.DrawWireCube(transform.position - new Vector3(0, transform.localScale.y + bottomOffset, 0), new Vector2(bottomXSize, bottomYSize));
+        Gizmos.DrawWireCube(transform.position - new Vector3(transform.localScale.x/2 + leftOffset, 0, 0), new Vector2(leftXSize, leftYSize));
+        Gizmos.DrawWireCube(transform.position + new Vector3(transform.localScale.x/2 + rightOffset, 0, 0), new Vector2(rightXSize, rightYSize));
     }
 }
 
